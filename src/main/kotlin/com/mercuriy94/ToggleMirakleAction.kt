317e31b836d237293198d75eb7e4613c676b7c87
Marsyukov Nikita <mercuriy94@gmail.com>
2021-03-20 16:00:43 +0300

    Fix work local.properties in multimodules project

--- src/main/kotlin/com/mercuriy94/ToggleMirakleAction.kt
+++ src/main/kotlin/com/mercuriy94/ToggleMirakleAction.kt
@@ -5,20 +5,20 @@ import com.intellij.lang.properties.psi.impl.PropertiesFileImpl
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.openapi.roots.ProjectRootManager
-import com.intellij.psi.PsiManager
+import com.intellij.psi.search.FilenameIndex.getFilesByName
+import com.intellij.psi.search.GlobalSearchScope.projectScope
 import icons.PluginIcons
 
 class ToggleMirakleAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val localProps = getLocalPropertiesFile(e) ?: return
-        val property = localProps.findPropertyByKey("mirakle_enabled") ?: return
+        val property = localProps.findPropertyByKey(MIRAKLE_ENABLE_PROPERTY_KEY) ?: return
         val currentValue = property.value
-        val (newIcon, newValue) = if (currentValue == "true") {
-            PluginIcons.MIRAKLE_OFF to "false"
+        val (newIcon, newValue) = if (currentValue == ENABLE_MIRAKLE) {
+            PluginIcons.MIRAKLE_OFF to DISABLE_MIRAKLE
         } else {
-            PluginIcons.MIRAKLE_ON to "true"
+            PluginIcons.MIRAKLE_ON to ENABLE_MIRAKLE
         }
         e.presentation.icon = newIcon
         WriteCommandAction.runWriteCommandAction(e.project) { property.setValue(newValue) }
@@ -27,21 +27,28 @@ class ToggleMirakleAction : AnAction() {
     override fun update(e: AnActionEvent) {
         super.update(e)
         val localProps = getLocalPropertiesFile(e) ?: return
-        val property = localProps.findPropertyByKey("mirakle_enabled") ?: return
+        val property = localProps.findPropertyByKey(MIRAKLE_ENABLE_PROPERTY_KEY) ?: return
         val currentValue = property.value
-        if (currentValue == "true") {
+        if (currentValue == ENABLE_MIRAKLE) {
             e.presentation.icon = PluginIcons.MIRAKLE_ON
         } else {
             e.presentation.icon = PluginIcons.MIRAKLE_OFF
         }
-
     }
 
     private fun getLocalPropertiesFile(e: AnActionEvent): PropertiesFile? {
         val project = e.project ?: return null
-        val vFiles = ProjectRootManager.getInstance(project).contentRoots
-        val virtualLocalProps = vFiles.first().children.find { it.name == "local.properties" } ?: return null
-        val psiLocalPropsFile = PsiManager.getInstance(project).findFile(virtualLocalProps) ?: return null
-        return PropertiesFileImpl(psiLocalPropsFile.viewProvider)
+        val localPropsViewProvider = getFilesByName(project, LOCAL_PROPS_FILE_NAME, projectScope(project))
+            .first { it.parent?.let { psiDir -> psiDir.virtualFile.path == project.basePath } ?: false }
+            ?.viewProvider
+            ?: return null
+        return PropertiesFileImpl(localPropsViewProvider)
+    }
+
+    private companion object {
+        const val ENABLE_MIRAKLE = "true"
+        const val DISABLE_MIRAKLE = "false"
+        const val LOCAL_PROPS_FILE_NAME = "local.properties"
+        const val MIRAKLE_ENABLE_PROPERTY_KEY = "mirakle_enabled"
     }
 }
